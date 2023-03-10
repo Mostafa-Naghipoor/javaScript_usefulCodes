765807cf875f1bcf91be7c4cdc96dd3f0e7e7737
Mostafa Naghipoor <89272100+MostafaNaghipoor82@users.noreply.github.com>
2023-03-09 22:47:19 -0800

    Write a function that takes a string (a) as argument. Extract the first half a list

--- nth_character.js
+++ nth_character.js
@@ -7,8 +7,16 @@ console.log(myFunction("elimo", 2))
 
 
 
-//Write a function that takes a string (a) as argument. Remove the first 3 characters of a
+//Write a function that takes a string (a) as argument. Remove the first 3 characters of a list
 
 function myFunction(a) {
     return a.slice(3);
  }
+
+
+
+ //Write a function that takes a string (a) as argument. Extract the first half a list
+ function extractFirstHalf(a) {
+    const halfLength = Math.floor(a.length / 2);
+    return a.slice(0, halfLength);
+  }
