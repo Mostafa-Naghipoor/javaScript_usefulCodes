3ea2c175a81dd6a99955a15bc545ac8666923833
Mostafa Naghipoor <89272100+MostafaNaghipoor82@users.noreply.github.com>
2023-03-09 22:45:41 -0800

    Write a function that takes a string (a) as argument. Remove the first 3 characters of a list

--- nth_character.js
+++ nth_character.js
@@ -4,3 +4,11 @@ function myFunction(a, n){
 return a.charAt(n)
 }
 console.log(myFunction("elimo", 2))
+
+
+
+//Write a function that takes a string (a) as argument. Remove the first 3 characters of a
+
+function myFunction(a) {
+    return a.slice(3);
+ }
