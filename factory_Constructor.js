ba8746d48e12eaa27837500e504261dd0e4c451a
Mostafa Naghipoor <89272100+MostafaNaghipoor82@users.noreply.github.com>
2023-04-12 23:30:12 -0700

    Check if a value is a valid postal code:

--- factory_Constructor.js
+++ factory_Constructor.js
@@ -22,3 +22,17 @@ function CreateAddress(street,city, zipCode) {
 }
  let Address = new CreateAddress("a","b","c");
   console.log(Address)
+
+  // Check if a value is a valid postal code:
+  const isPostalCode = (postalCode, countryCode) => {
+  if (countryCode === 'US') {
+    const regex = /[0-9]{5}(?:-[0-9]{4})?/;
+    return regex.test(postalCode);
+  } else if (countryCode === 'CA') {
+    const regex = /[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ] [0-9][ABCEGHJKLMNPRSTVWXYZ][0-9]/;
+    return regex.test(postalCode.toUpperCase());
+  } else {
+    // Add regex for other country codes as needed
+    return false;
+  }
+}
